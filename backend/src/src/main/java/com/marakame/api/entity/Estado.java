36e55be16573c5a560079fa61a4e36b1233032af
JGabrielGlez <jegagonzalezes@ittepic.edu.mx>
2026-04-22 22:10:34 -0700

    Endpoint para el get requisiciones devuelve todos los datos de estas, completos

--- backend/src/src/main/java/com/marakame/api/entity/Estado.java
+++ backend/src/src/main/java/com/marakame/api/entity/Estado.java
@@ -1,7 +1,21 @@
 package com.marakame.api.entity;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Estado {
     PENDIENTE,
     PRE_AUTORIZADA,
     AUTORIZADA,
-    FINALIZADA
+    FINALIZADA;
+
+    @JsonValue
+    public String toJson() {
+        return this.name().replace('_', '-');
+    }
+
+    @JsonCreator
+    public static Estado fromJson(String value) {
+        return Estado.valueOf(value.replace('-', '_'));
+    }
 }
